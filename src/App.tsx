8f21de7f729255ab1a45d7a0fb6036275d365b2d
Owen Dearman <owendearman@gmail.com>
2022-05-30 16:18:31 +0100

    added useReducer instead of useState to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 import { Header } from "./components/Header";
 import { MainDashboard } from "./components/main-dashboard/MainDashboard";
-import { fullProjectInterface, projectInterface } from "./utils/interfaces";
+import { fullProjectInterface } from "./utils/interfaces";
 import axios from "axios";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { addClientsAndEmployeesToProjects } from "./utils/addClientsAndEmployeesToProjects";
@@ -9,17 +9,54 @@ import { EmployeeDashboard } from "./components/EmployeeDashboard";
 import { ClientDashboard } from "./components/ClientDashboard";
 
 function App(): JSX.Element {
-  const [projectData, setProjectData] = useState<fullProjectInterface[]>([]);
+  type State = {
+    data: fullProjectInterface[];
+    isLoading: boolean;
+    filters?: {
+      projectSize: { min: null; max: null };
+      clients: [];
+      employees: [];
+      timeFrame: { start: null; end: null };
+      employeeNumber: { min: null; max: null };
+    };
+  };
+
+  type Action =
+    | { type: "request" }
+    | { type: "success"; results: fullProjectInterface[] };
+
+  const projectDataReducer = (state: State, action: Action): State => {
+    switch (action.type) {
+      case "request":
+        return {
+          ...state,
+          isLoading: true,
+        };
+      case "success":
+        return {
+          ...state,
+          isLoading: false,
+          data: action.results,
+        };
+    }
+  };
+
+  const [{ data, isLoading }, dispatch] = useReducer(projectDataReducer, {
+    data: [],
+    isLoading: false,
+  });
+
+  //const [projectData, setProjectData] = useState<fullProjectInterface[]>([]);
 
   useEffect(() => {
-    async function fetchProjects() {
-      const projects = await axios.get(
-        "https://consulting-projects.academy-faculty.repl.co/api/projects"
-      );
-      const projectData: projectInterface[] = projects.data;
-      const fullProjectInformation: fullProjectInterface[] =
-        await addClientsAndEmployeesToProjects(projectData);
-      setProjectData(fullProjectInformation);
+    function fetchProjects() {
+      dispatch({ type: "request" });
+      axios
+        .get("https://consulting-projects.academy-faculty.repl.co/api/projects")
+        .then((results) => addClientsAndEmployeesToProjects(results.data))
+        .then((projectData) =>
+          dispatch({ type: "success", results: projectData })
+        );
     }
     fetchProjects();
   }, []);
@@ -28,20 +65,21 @@ function App(): JSX.Element {
     <>
       <Router>
         <Header />
-        <Routes>
-          <Route
-            path="/"
-            element={<MainDashboard projectData={projectData} />}
-          />
-          <Route
-            path="/employees/:employeeId"
-            element={<EmployeeDashboard projectData={projectData} />}
-          />
-          <Route
-            path="/clients/:clientId"
-            element={<ClientDashboard projectData={projectData} />}
-          />
-        </Routes>
+        {isLoading ? (
+          <h1 className="loadingMessage">Loading...</h1>
+        ) : (
+          <Routes>
+            <Route path="/" element={<MainDashboard projectData={data} />} />
+            <Route
+              path="/employees/:employeeId"
+              element={<EmployeeDashboard projectData={data} />}
+            />
+            <Route
+              path="/clients/:clientId"
+              element={<ClientDashboard projectData={data} />}
+            />
+          </Routes>
+        )}
       </Router>
     </>
   );
